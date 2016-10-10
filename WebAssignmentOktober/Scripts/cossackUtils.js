bccac0d12d2d89da08918acd58e2015768f72600
Cossack-HD <kazakhd@gmail.com>
2016-10-10 12:48:26 +0200

    Added js sliding function and moved it to separate files which are linked to index.cshtml

---
+++ WebAssignmentOktober/Scripts/cossackUtils.js
@@ -0,0 +1,28 @@
+﻿slides_num = 1;
+current_slide = 0;
+
+function initSlides(timer) {
+    if (current_slide < slides_num) {
+        window.setTimeout(moveSlides, timer);
+    }
+    else {
+        window.setTimeout(moveSlidesDefault, timer);
+        current_slide = 0;
+    }
+}
+
+
+function moveSlides() {
+    current_slide++;
+    $('.inner-window').animate({
+        right: "+=300px"
+    }, 2000);
+    initSlides(6000);
+}
+
+function moveSlidesDefault() {
+    $('.inner-window').animate({
+        right: "0px"
+    }, 2000);
+    initSlides(6000);
+}
